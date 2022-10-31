4392132aab25ba78c185b796e261114f9e25daed
Chang Chen <shchenchang@gmail.com>
2022-10-31 16:04:50 +0800

    Disable `MVP` usage.

--- triangle-opengles.c
+++ triangle-opengles.c
@@ -42,11 +42,15 @@ typedef struct Vertex
     vec3 col;
 } Vertex;
 
-static const Vertex vertices[3] =
+static const Vertex vertices[6] =
 {
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+    { { -1.0f, -1.0f }, { 1.f, 0.f, 0.f } },
+    { {  1.0f, -1.0f }, { 0.f, 1.f, 0.f } },
+    { { -1.0f,  1.0f }, { 0.f, 0.f, 1.f } },
+
+    { { -1.0f,  1.0f }, { 0.f, 0.f, 1.f } },
+    { {  1.0f, -1.0f }, { 0.f, 1.f, 0.f } },
+    { {  1.0f,  1.0f }, { 1.f, 1.f, 0.f } },
 };
 
 static const char* vertex_shader_text =
@@ -54,11 +58,12 @@ static const char* vertex_shader_text =
 "precision mediump float;\n"
 "uniform mat4 MVP;\n"
 "attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
+"attribute vec3 vPos;\n"
 "varying vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+//"    gl_Position = MVP * vec4(vPos, 1.0);\n"
+"    gl_Position = vec4(vPos, 1.0);\n"
 "    color = vCol;\n"
 "}\n";
 
@@ -246,29 +251,29 @@ int main(int argc, char** argv)
 
     glEnableVertexAttribArray(vpos_location);
     glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+    glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, pos));
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, col));
 
     while (!glfwWindowShouldClose(window))
     {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        const float ratio = width / (float) height;
+        //int width, height;
+        //glfwGetFramebufferSize(window, &width, &height);
+        //const float ratio = width / (float) height;
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
         mat4x4 m, p, mvp;
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
+        //mat4x4_identity(m);
+        //mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        //mat4x4_mul(mvp, p, m);
 
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
